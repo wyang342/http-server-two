2c3d10fb48dd18664f25b49e8f9e1541cfc32e74
Jonathan Young <jonyoungg@gmail.com>
2019-02-06 13:58:04 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import datetime
 from request import Request
 
 def build_html_response(text_body):
-  html_body = f'<html><head><title>An Example Page</title></head><body>{text_body}</body></html>'
+  html_body = f"<html><head><title>An Example Page</title></head><body>{text_body}</body></html>"
   return f"HTTP/1.1 200 OK\r\nContent-Type:text/html\r\nContent-Length:{len(html_body)}\r\n\r\n{html_body}"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
